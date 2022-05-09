edf9677b4e4f85d590039915975de1c175fde968
unknown <mrfelipesensei@gmail.com>
2022-05-09 17:26:06 -0300

    Adicionando poor herança

---
+++ Vistiante.php
@@ -0,0 +1,11 @@
+<?php 
+
+    require_once 'Pessoas.php';
+
+    class Visitante extends Pessoas{
+
+    }
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
+require_once 'Vistiante.php';
 require_once 'Aluno.php';
 require_once 'Atleta.php';
 require_once 'Personal.php';
+
 
 $p1= new Personal("Yoko",28,"F");
 $p1->aniversario();
@@ -13,7 +15,11 @@ $a1->pagar();
 $at = new Atleta("Homer",47,"M");
 $at->pagar();
 
-print_r($p1);
+$v = new Visitante("Dennis",54,"M");
+
+
+print_r($v);
+
 
 
 
